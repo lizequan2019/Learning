4e05cdc1890e6489821c7d90c732475c5a3325aa
lizequan <2549524215@qq.com>
2024-06-23 22:49:15 +0800

    更新文件

---
+++ notescheck/mainwindow.h
@@ -0,0 +1,28 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDir>
+#include <qDebug>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_btn_markdown_ch_clicked();
+
+    void on_btn_filelink_ch_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
